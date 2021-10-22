2e11aa457ab4668819654873c64f3b464bf40ea4
Agung <agungskripsi05@gmail.com>
2021-10-22 19:24:43 +0700

    hello

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get("hello", function() {
+    return [
+        "message" => "hello"
+    ];
+});
+
 Route::group(['middleware' => 'auth:api'], function () {
     Route::post('logout', [LoginController::class, 'logout']);
 
